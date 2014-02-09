93d02a818448a484ef9822b60d2416a535857c88
Vittorio Scibetta <vittorioscibetta@fico.com>
2014-02-09 18:56:50 +0000

    distinctions.hs

---
+++ chapter3/distinctions.hs
@@ -0,0 +1,9 @@
+-- file: ch03/distinctions.hs
+a = ("Porpoise", "Grey")
+b = ("Table", "Oak")
+
+data Cetacean = Cetacean String String
+data Furniture = Furniture String String
+
+c = Cetacean "Porpoise" "Grey"
+d = Furniture "Table" "Oak"
