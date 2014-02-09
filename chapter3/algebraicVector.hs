fb6793ec892d9fa74b1f034dbb2eaa6a4bb856cf
Vittorio Scibetta <vittorioscibetta@fico.com>
2014-02-09 19:07:41 +0000

    chp3 - Tuples, algebraic data types, and when to use each

---
+++ chapter3/algebraicVector.hs
@@ -0,0 +1,9 @@
+-- file: ch03/algebraicVector.hs
+-- x and y coordinates or lengths.
+
+data Cartesian2D= Cartesian2D Double Double 
+			deriving (Eq, Show)
+
+-- Angle and distance (magnitude)
+data Polar2D = Polar2D Double Double
+		deriving (Eq, Show)
