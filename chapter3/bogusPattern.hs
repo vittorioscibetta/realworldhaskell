bbaca91763a9aba2c5777bcf8196f79023abc9fd
Vittorio Scibetta <vittorioscibetta@fico.com>
2014-02-11 00:09:43 +0000

    ch3 - case expression

---
+++ chapter3/bogusPattern.hs
@@ -0,0 +1,23 @@
+--file: ch03/bogusPattern.hs
+
+data Fruit = Apple | Orange deriving (Show)
+
+apple = "apple"
+orange = "orange"
+
+whichFruit :: String -> Fruit
+
+whichFruit f = case f of
+		apple -> Apple
+		orange -> Orange
+
+
+
+equational apple = Apple
+equational orange = Orange
+
+
+
+betterFruit f = case f of
+		"apple" -> Apple
+		"orange" -> Orange
